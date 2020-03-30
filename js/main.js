3e1b56f9a636ec6cdf4b5ce99ee3455d6ad53079
DaunPictures <solodovnikov.toni@gmail.com>
2020-03-30 13:39:42 +0300

    Pre-Alpha 1.4
    Almost everything is ready.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 /* Burger */
+
 $(document).ready(function() {
 	$('.header__burger').click(function(event){
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 });
+
+/*Parallax */
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+  relativeInput: true
+});
+
+parallaxInstance.friction(0.2, 0.2);
